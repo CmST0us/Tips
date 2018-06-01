ecdeabafd78d86bd19afde0311ea78933cf675d6
Meskjei <513087078@qq.com>
2018-06-01 23:31:28 +0800

    Add files via upload
    主页添加关注按钮

--- home/home.js
+++ home/home.js
@@ -1,6 +1,5 @@
 // home/home.js
 var amapFile = require('../libs/amap-wx.js');
-var markersData = [];
 var app = getApp();
 Page({
 
@@ -9,14 +8,14 @@ Page({
    */
   data: {
     tipData: [],
-    currentTab: 0 
+    currentTab: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
   swichNav: function (e) {
     var that = this;
@@ -33,7 +32,7 @@ Page({
       currentTab: e.detail.current,
     })
   },
-  showDetail: function(e){
+  showDetail: function (e) {
     console.log(e.currentTarget.dataset.tip);
     app.globalData.currentTip = e.currentTarget.dataset.tip;
     wx.navigateTo({
@@ -45,7 +44,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
@@ -76,45 +75,92 @@ Page({
       }
     })
   },
+  catchFollow: function (e) {
+    let tip = e.currentTarget.dataset.tip;
+    console.log(tip);
+    let tableID = app.globalData.tableID.follow;
+    let uid = wx.BaaS.storage.get('uid');
+    let followTableObject = new wx.BaaS.TableObject(tableID);
+    let query = new wx.BaaS.Query();
+    query.compare('created_by', '=', uid);
+    followTableObject.setQuery(query).find().then(function (res) {
+      let followTableObject = new wx.BaaS.TableObject(tableID);
+      console.log(res);
+      if (res.data.objects.length == 0) {
+        let followRecord = followTableObject.create();
+        followRecord.set({
+          myFollowTips: [tip.id]
+        }).save().then(function (saveRes) {
+          console.log(saveRes);
+        }, function (saveErr) {
+          console.log(saveErr);
+        })
+
+      } else {
+        console.log('else');
+        let followList = res.data.objects[0].myFollowTips;
+        if (followList.indexOf(tip.id) != -1) {
+          wx.showToast({
+            title: '您已关注',
+          });
+        } else {
+          console.log(followList);
+          followList.push(tip.id);
+          console.log(followList);
+          let followRecord = followTableObject.getWithoutData(res.data.objects[0].id);
+          followRecord.set({
+            myFollowTips: followList
+          });
+          followRecord.update().then(function (res) {
+            console.log(res);
+          }, function (err) {
+            console.log(err);
+          })
+        }
+      }
+    }, function (err) {
+
+    })
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
   userInfoHandler(data) {
     console.log(data)
     wx.BaaS.handleUserInfo(data).then(res => {
-      console.log(res);  
+      console.log(res);
     }, res => {
       console.log(res);
     })
