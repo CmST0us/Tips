806bdeb186fd8d23967e490962b4d92b584da947
CmST0us <eric3u@outlook.com>
2018-06-02 22:28:09 +0800

    添加我的页面的上拉加载，下拉刷新。对于#2使用调整高度的方法暂时修复

--- my/my.js
+++ my/my.js
@@ -9,7 +9,9 @@ Page({
     isLoginIn: false,
     myTaskList: [],
   },
-
+  currentPage: 0,
+  rowPerPage: 20,
+  hasNext: true,
   /**
    * 生命周期函数--监听页面加载
    */
@@ -53,6 +55,46 @@ Page({
       console.log(err);
     });
   },
+  fetchDataAtCurrentPageAndRender: function() {
+    let that = this;
+    let app = getApp();
+    let userTaskListTableId = app.globalData.tableID.userTaskList;
+    let query = new wx.BaaS.Query();
+    let uid = that.data.userInfo.id;
+    query.compare('created_by', '=', uid);
+    let tableObject = this.createTableObject(userTaskListTableId, query);
+
+    tableObject.find().then(res => {
+      console.log(res);
+
+      let objects = res.data.objects;
+      // 请求objects数据库
+      if (objects != null && objects.length > 0) {
+        let tipsTableId = app.globalData.tableID.tips;
+        let tipQuery = new wx.BaaS.Query();
+        tipQuery.in('id', objects[0].myTaskList);
+        let tipTableObject = that.createTableObject(tipsTableId, tipQuery, that.currentPage);
+
+        tipTableObject.find().then(res => {
+          if (res.data.meta.next == null) {
+            that.hasNext = false;
+          } else {
+            that.hasNext = true;
+          }
+
+          let taskList = that.data.myTaskList.concat(res.data.objects);
+          that.setData({
+            myTaskList: taskList
+          });
+
+        }, e => {
+          console.log(e);
+        });
+      }
+    }, err => {
+      console.log(err);
+    });
+  },
   userInfoHandler(data) {
     let that = this;
     wx.BaaS.handleUserInfo(data).then(res => {
@@ -74,7 +116,23 @@ Page({
       });
     })
   },
-
+  // 创建TableObject对象
+  // id: table ID
+  // query: wx.BaaS.Query 对象
+  // pageNum: 请求的页面
+  createTableObject: function (tableId, query, pageNum) {
+    if (pageNum == undefined) {
+      pageNum = 0;
+    }
+    if (query == undefined) {
+      query = new wx.BaaS.Query();
+    }
+    let limit = this.rowPerPage;
+    let offset = pageNum * limit;
+    let tipObejct = new wx.BaaS.TableObject(tableId);
+    tipObejct.setQuery(query).limit(limit).offset(offset);
+    return tipObejct;
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -107,14 +165,19 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.currentPage = 0;
+    this.data.myTaskList = [];
+    this.fetchDataAtCurrentPageAndRender();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    if (this.hasNext == true) {
+      this.currentPage += 1;
+      this.fetchDataAtCurrentPageAndRender();
+    }
   },
 
   /**
